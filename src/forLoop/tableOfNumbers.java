68432ee30f999e0310773719b9fc34cdf77a3983
dryigit <fatihyigit6@gmail.com>
2021-12-26 00:58:50 +0300

    Lambda

---
+++ src/forLoop/tableOfNumbers.java
@@ -0,0 +1,16 @@
+package forLoop;
+
+public class tableOfNumbers {
+
+public static void main(String[] args) {
+	//Java Program to demonstrate the example of for loop
+//which prints table of 1
+	int carpim=1;
+	for (int i = 1; i <= 10; i++) {
+		System.out.println(i +"*1 ="+ i);
+		
+		
+	}
+	
+}
+}
