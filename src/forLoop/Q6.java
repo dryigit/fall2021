9371f3c67398891240c37cbc872575d618922daf
dryigit <fatihyigit6@gmail.com>
2021-12-27 00:13:29 +0300

    number and letter value of the letters

---
+++ src/forLoop/Q6.java
@@ -0,0 +1,15 @@
+package forLoop;
+
+public class Q6 {
+public static void main(String[] args) {
+	// Print the number and letter value of the letters from 0 to 255 to the console.
+	
+	int num=255;
+	for (int i=0; i<num; i++) {
+		char harf= (char)i;
+		
+		System.out.println(i +" -->" +harf);
+		
+	}
+}
+}
